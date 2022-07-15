55f77b71199dc32a2ebeba40fe553256980a0713
tetuaoro <tetuaoropro@gmail.com>
2022-07-15 08:56:53 -1000

    fix: site web manifest
    * replaced image files
    * added 144x144 to manifest file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="preload" href="/images/motif_footer_white.svg" as="image" type="image/svg+xml" />
         <link rel="preload" href="/fonts/conthrax-sb.ttf" as="font" type="font/ttf" crossOrigin="" />
