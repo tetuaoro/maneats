7d13b6beb2f8fb2d8d52f860ad77ab0114b7bb20
tetuaoro <tetuaoropro@gmail.com>
2022-07-21 14:07:10 -1000

    fix: service worker
    * did not register

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-const withWorkbox = require("next-with-workbox");
+const withWorkbox = require("next-with-workbox")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withWorkbox({
   workbox: {
+    dest: "public",
+    swDest: "sw.js",
     swSrc: "worker.js",
+    force: true,
   },
   reactStrictMode: true,
 })
